463b005e6e3bd7529aa8efe65d327307a4b24ae5
dedtri <30638362+dedtri@users.noreply.github.com>
2022-07-07 09:19:05 +0200

    Add project files.

---
+++ MovieWeb/Models/Movies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieWeb.Models
+{
+    public class Movies
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        public string? Director { get; set; }
+
+        [Display(Name = "Release Year")]
+        public int ReleaseYear { get; set; }
+    }
+}
